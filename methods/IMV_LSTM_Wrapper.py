c4fea8df8b177f154df8043dda244035587cde1d
fmitharibeiro <francisco.mitha.ribeiro@tecnico.ulisboa.pt>
2024-04-19 11:33:25 +0100

    Imports

--- methods/IMV_LSTM_Wrapper.py
+++ methods/IMV_LSTM_Wrapper.py
@@ -1,9 +1,12 @@
 from .IMV_LSTM import IMVTensorLSTM, IMVFullLSTM
 import torch
+from torch.utils.data import DataLoader, TensorDataset
+from sklearn.model_selection import train_test_split
+from tqdm import tqdm
 
 
 class IMV_LSTM_Wrapper:
-    def __init__(self, input_dim, output_dim, num_hidden_layers, init_std=0.02):
+    def __init__(self, num_hidden_layers, init_std=0.02):
         self.model = None
         self.num_hidden_layers = num_hidden_layers
         self.init_std = init_std
@@ -12,16 +15,18 @@ class IMV_LSTM_Wrapper:
     
     def fit(self, X, y, batch_size=32, epochs=10):
         X_tensor = torch.FloatTensor(X).to(self.device)
-        y_tensor = torch.FloatTensor(y).unsqueeze(1).to(self.device)  # Assuming y is 1D array
+        y_tensor = torch.FloatTensor(y).to(self.device)
 
+        # To check
         X_train, X_val, y_train, y_val = train_test_split(X_tensor, y_tensor, test_size=0.2, random_state=42)
 
         train_dataset = TensorDataset(X_train, y_train)
         train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
 
         input_dim = X_tensor.shape[1]
+        output_dim = y_tensor.shape[1]
         self.model = IMVTensorLSTM(input_dim, output_dim, self.num_hidden_layers, self.init_std).to(self.device)
-        criterion = nn.MSELoss()
+        criterion = torch.nn.MSELoss()
         optimizer = torch.optim.Adam(self.model.parameters()) # learning rate?
 
         for epoch in range(epochs):

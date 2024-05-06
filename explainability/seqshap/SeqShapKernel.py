0148fbc2411b736d0c32793afe0568d0c8f4f5f5
fmitharibeiro <francisco.mitha.ribeiro@tecnico.ulisboa.pt>
2024-05-06 23:19:20 +0100

    SeqSHAP (concept)

--- explainability/seqshap/SeqShapKernel.py
+++ explainability/seqshap/SeqShapKernel.py
@@ -1,5 +1,6 @@
 import numpy as np
 from .segmentation import SeqShapSegmentation
+from shap import KernelExplainer
 
 class SeqShapKernel:
     def __init__(self, model, seq_num, dataset_name, background="feat_mean", random_seed=None, f=None):
@@ -34,6 +35,9 @@ class SeqShapKernel:
 
         segmented_X = seg(X)
         self.k = segmented_X.shape[0]
+
+        # Eq. 8
+        self.phi_f = self.compute_feature_explanations(segmented_X)
         pass
 
     def compute_background(self, X, method):
@@ -41,4 +45,62 @@ class SeqShapKernel:
             return np.mean(X, axis=0)
         else:
             raise NotImplementedError
+    
+    def compute_feature_explanations(self, subsequences):
+        phi_f = np.zeros(subsequences[0].shape[1])  # Initialize feature-level explanations
+        self.phi_seq = []
+
+        # Iterate over each subsequence
+        for subseq in subsequences:
+            explainer = KernelExplainer(self.f, self.background)
+            shap_values = explainer.shap_values(subseq)  # TODO: Check if it works as intended
+
+            # Sum the Shapley values for each feature
+            phi_f += np.sum(shap_values, axis=0)
+            self.phi_seq.append(np.sum(shap_values, axis=0))
+
+        return phi_f
+    
+    def compute_subsequence_explanations(self, X_prime):
+        K, M = X_prime.shape
+        phi = np.zeros((K, M))  # Initialize subsequence-level explanations
+
+        for j in range(M):
+            Sj = X_prime[:, j].reshape(-1, 1)  # Extract subsequence Sj
+            zj = np.ones((K,))  # zj is a binary vector representing the presence of the jth feature in the subsequence
+
+            # Define hx(zj) and g(zj) using some functions (Equations 9 and 10)
+            hx_zj = self.hx_function(X_prime, zj, j)
+            g_zj = self.g_function(zj, j)
+
+            # Use KernelSHAP to compute the Shapley values for the jth feature in the subsequence
+            explainer = KernelExplainer(self.f, self.background)
+            phi_j = explainer.shap_values(Sj, zj, hx_zj, g_zj) # TODO: Only Sj is accepted, the rest may need integration
+
+            # Store the Shapley values for the jth feature
+            phi[:, j] = phi_j
+
+        return phi
+
+    def hx_function(self, X_prime, zj, j):
+        K, M = X_prime.shape
+        hx = np.ones((K, M))  # Initialize hx matrix with ones
+
+        # Replace column j with zj
+        hx[:, j] = zj
+
+        # Replace 1s with X_prime[i, j] and 0s with background[j]
+        hx = np.where(hx == 1, X_prime, self.background)
+
+        return hx
+
+    def g_function(self, zj, j):
+        # Compute phi_seq_0_j using background values
+        phi_f_0 = np.mean(self.phi_f, axis=0)  # Mean of feature-level attributions
+        phi_seq_0_j = phi_f_0 + np.sum(self.phi_f) - self.phi_f[j]  # phi_seq_0_j calculation
+
+        # Compute g_seq_j
+        g_seq_j = phi_seq_0_j + np.sum(self.phi_seq[j] * zj, axis=0)
+
+        return g_seq_j
 
